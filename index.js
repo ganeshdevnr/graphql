83698db77e71f47000cdc0db3635196e340a4bf4
ganeshnr-frequence <ganesh.nr@frequence.com>
2023-04-15 15:28:13 +0530

    Setup first graphql query

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing');
+});
+
+// created a response
+const root = {
+    hello: () => "Hi, I'm GraphQL"
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(8080, () => console.log('Running server on port localhost:8080'));
